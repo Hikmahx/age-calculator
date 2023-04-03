c5669c7be04e18798ddf1c68611178b6a459ead1
Hikmahx <hikmayousuph@gmail.com>
2023-04-03 07:32:12 +0100

    age input desktop view

--- src/components/AgeCard.tsx
+++ src/components/AgeCard.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import AgeInput from "./ageInput";
+import AgeInput from "./AgeInput";
 
 const AgeCard = () => {
   return (
-    <div className="bg-white w-full max-w-md lg:max-w-[840px] h-[486px] rounded-[22px] rounded-br-[102px] mt-2 lg:mt-0 py-11 px-6">
+    <div className="bg-white w-full max-w-md lg:max-w-[840px] h-[486px] lg:h-[651px] rounded-[22px] rounded-br-[102px] lg:rounded-br-[200px] mt-2 lg:mt-0 py-11 lg:py-12 px-6 lg:px-14 ">
       <AgeInput />
     </div>
   );
--- src/components/AgeInput.tsx
+++ src/components/AgeInput.tsx
@@ -2,46 +2,46 @@ import React from "react";
 
 const AgeInput = () => {
   return (
-    <form className="font-poppins mt-[5px] flex items-center justify-between max-w-[300px]">
+    <form className="font-poppins mt-[5px] lg:mt-2 flex items-center justify-between max-w-[300px] lg:max-w-[544px]">
       <div className="flex flex-col">
         <label
           htmlFor="day"
-          className="text-smokey-grey uppercase font-poppins text-xs tracking-[0.25em] mb-1"
+          className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
         >
           day
         </label>
         <input
           type="text"
           id="day"
-          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] h-[54px] px-3.5 text-xl"
+          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
           placeholder="DD"
         />
       </div>
       <div className="flex flex-col">
         <label
           htmlFor="month"
-          className="text-smokey-grey uppercase font-poppins text-xs tracking-[0.25em] mb-1"
+          className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
         >
           month
         </label>
         <input
           type="text"
           id="month"
-          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] h-[54px] px-3.5 text-xl"
+          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
           placeholder="MM"
         />
       </div>
       <div className="flex flex-col">
         <label
           htmlFor="year"
-          className="text-smokey-grey uppercase font-poppins text-xs tracking-[0.25em] mb-1"
+          className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
         >
           year
         </label>
         <input
           type="text"
           id="year"
-          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] h-[54px] px-3.5 text-xl"
+          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
           placeholder="YYYY"
         />
       </div>
