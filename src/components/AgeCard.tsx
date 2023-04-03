e2f2502e1c4d7378e0f6bd37a18e164bffdc2df8
Hikmahx <hikmayousuph@gmail.com>
2023-04-03 18:27:41 +0100

    divider transfer to AgeInput component

--- src/components/AgeCard.tsx
+++ src/components/AgeCard.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import AgeInput from "./AgeInput";
-import arrow from "../assets/icon-arrow.svg";
 import AgeResult from "./AgeResult";
 
 const AgeCard = () => {
   return (
     <div className="overflow-hidden bg-white w-full max-w-md lg:max-w-[840px] h-[486px] lg:h-[651px] rounded-[22px] rounded-br-[102px] lg:rounded-br-[200px] mt-2 lg:mt-0 py-11 lg:py-12 px-6 lg:px-14 ">
       <AgeInput />
-      <div className="divider border-b border-light-grey py-8 lg:py-6 relative">
-        <div className="absolute w-16 lg:w-24 h-16 lg:h-24 rounded-full bg-purple inset-x-0 lg:top-0 lg:left-auto mx-auto flex p-5 lg:p-6">
-          <img src={arrow} alt="down arrow" className="" />
-        </div>
-      </div>
       <AgeResult />
     </div>
   );
--- src/components/AgeInput.tsx
+++ src/components/AgeInput.tsx
@@ -1,49 +1,60 @@
 import React from "react";
+import arrow from "../assets/icon-arrow.svg";
 
 const AgeInput = () => {
   return (
-    <form className="font-poppins mt-[5px] lg:mt-2 flex items-center justify-between max-w-[300px] lg:max-w-[544px]">
-      <div className="flex flex-col">
-        <label
-          htmlFor="day"
-          className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
-        >
-          day
-        </label>
-        <input
-          type="text"
-          id="day"
-          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
-          placeholder="DD"
-        />
+    <form>
+      <div className="font-poppins mt-[5px] lg:mt-2 flex items-center justify-between max-w-[300px] lg:max-w-[544px]">
+        <div className="flex flex-col">
+          <label
+            htmlFor="day"
+            className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
+          >
+            day
+          </label>
+          <input
+            type="text"
+            id="day"
+            className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
+            placeholder="DD"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label
+            htmlFor="month"
+            className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
+          >
+            month
+          </label>
+          <input
+            type="text"
+            id="month"
+            className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
+            placeholder="MM"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label
+            htmlFor="year"
+            className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
+          >
+            year
+          </label>
+          <input
+            type="text"
+            id="year"
+            className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
+            placeholder="YYYY"
+          />
+        </div>
       </div>
-      <div className="flex flex-col">
-        <label
-          htmlFor="month"
-          className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
+      <div className="divider border-b border-light-grey py-8 lg:py-6 relative">
+        <button
+          type="submit"
+          className="absolute w-16 lg:w-24 h-16 lg:h-24 rounded-full bg-purple hover:bg-off-black transition-all inset-x-0 lg:top-0 lg:left-auto mx-auto flex p-5 lg:p-6"
         >
-          month
-        </label>
-        <input
-          type="text"
-          id="month"
-          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
-          placeholder="MM"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label
-          htmlFor="year"
-          className="text-smokey-grey uppercase font-poppins text-xs lg:text-sm tracking-[0.25em] mb-[5px] lg:mb-2"
-        >
-          year
-        </label>
-        <input
-          type="text"
-          id="year"
-          className="border border-light-grey rounded-md text-off-black placeholder-smokey-grey focus:outline-none focus:border-purple w-[88px] lg:w-40 h-[54px] lg:h-[72px] px-3.5 lg:px-6 text-xl lg:text-[2rem]"
-          placeholder="YYYY"
-        />
+          <img src={arrow} alt="down arrow" className="" />
+        </button>
       </div>
     </form>
   );
